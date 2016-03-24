bffaa76f2a8ef22761892feed908250939b23530
唐向龙 <tangxianglong@mr255.local>
2016-03-24 14:22:14 +0800

    Initial Commit

---
+++ Test_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test_C
+//
+//  Created by 唐向龙 on 16/3/24.
+//  Copyright © 2016年 唐向龙. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
